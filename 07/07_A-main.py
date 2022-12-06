8d52362c10004329e453a30d79e330811e1387ab
Aaron M <thisaaronm@outlook.com>
2022-12-05 21:59:46 -0800

    initial commit

---
+++ 07/07_A-main.py
@@ -0,0 +1,17 @@
+
+def get_puzzle_input(filename: str) -> list:
+    """
+    - returns puzzle input as list
+    """
+    with open(filename, 'r') as f:
+        characters = f.read()
+
+    return characters
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
