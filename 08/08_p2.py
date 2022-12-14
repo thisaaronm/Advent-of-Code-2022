9487973f1968b657ddd31ec2492817e40c23c523
Aaron M <thisaaronm@outlook.com>
2022-12-13 23:39:28 -0800

    clean up and var rename

--- 08/08_p2.py
+++ 08/08_p2.py
@@ -21,7 +21,7 @@ def create_grid(data: list[str]) -> list[list[int]]:
     return grid
 
 
-def check_visibility(grid: list[list[int]]) -> int:
+def get_scenic_score(grid: list[list[int]]) -> int:
     '''
     x => horizontal => columns
     y => vertical   => rows
@@ -31,111 +31,99 @@ def check_visibility(grid: list[list[int]]) -> int:
         r_idx, row_data = row ## (row_index / grid[row_index])
 
         for column in enumerate(row_data):
-            tree_idx, curr_tree = column ## (column_index / grid[row_index][column_index])
+            cur_tree_idx, cur_tree_val = column ## (column_index / grid[row_index][column_index])
 
-            VIEW_LEFT   = 0
-            VIEW_RIGHT  = 0
-            VIEW_TOP    = 0
-            VIEW_BOTTOM = 0
+            VIEW_L = 0
+            VIEW_R = 0
+            VIEW_T = 0
+            VIEW_B = 0
 
             ## Tree to Left
-            if tree_idx == 0:
+            if cur_tree_idx == 0:
                 pass
             else:
-                left_tree_idx = tree_idx - 1
+                l_tree_idx = cur_tree_idx - 1
                 check = True
                 while check:
-                    print(f'[{r_idx}, {tree_idx}], [{curr_tree}], {left_tree_idx}, {grid[r_idx][left_tree_idx]}')
-                    tree_to_the_left = grid[r_idx][left_tree_idx]
-                    if tree_to_the_left < curr_tree:
-                        VIEW_LEFT += 1
+                    l_tree_val = grid[r_idx][l_tree_idx]
+                    if l_tree_val < cur_tree_val:
+                        VIEW_L += 1
                     else:
-                        VIEW_LEFT += 1
+                        VIEW_L += 1
                         check = False
 
-                    if left_tree_idx <= 0:
+                    if l_tree_idx <= 0:
                         check = False
-                    left_tree_idx -= 1
-            print(f'\nVIEW_LEFT: {VIEW_LEFT}')
+                    l_tree_idx -= 1
             
             # ## Tree to Right
-            if tree_idx == len(row_data) - 1:
+            if cur_tree_idx == len(row_data) - 1:
                 pass
             else:
-                right_tree_idx = tree_idx + 1
+                r_tree_idx = cur_tree_idx + 1
                 check = True
                 while check:
-                    print(f'[{r_idx}, {tree_idx}], [{curr_tree}], {right_tree_idx}, {grid[r_idx][right_tree_idx]}')
-                    tree_to_the_right = grid[r_idx][right_tree_idx]
-                    if tree_to_the_right < curr_tree:
-                        VIEW_RIGHT += 1
+                    r_tree_val = grid[r_idx][r_tree_idx]
+                    if r_tree_val < cur_tree_val:
+                        VIEW_R += 1
                     else:
-                        VIEW_RIGHT += 1
+                        VIEW_R += 1
                         check = False
                     
-                    if right_tree_idx >= len(row_data) - 1:
+                    if r_tree_idx >= len(row_data) - 1:
                         check = False
-                    right_tree_idx += 1
-            print(f'VIEW_RIGHT: {VIEW_RIGHT}')
+                    r_tree_idx += 1
 
             # ## Tree to Bottom
             if r_idx == len(row_data) - 1:
                 pass
             else:
-                bottom_tree_idx = r_idx + 1
+                b_tree_idx = r_idx + 1
                 check = True
                 while check:
-                    print(f'[{r_idx}, {tree_idx}], [{curr_tree}], {bottom_tree_idx}, {grid[r_idx][bottom_tree_idx]}')
-                    tree_to_the_bottom = grid[bottom_tree_idx][tree_idx]
-                    if tree_to_the_bottom < curr_tree:
-                        VIEW_BOTTOM += 1
+                    b_tree_val = grid[b_tree_idx][cur_tree_idx]
+                    if b_tree_val < cur_tree_val:
+                        VIEW_B += 1
                     else:
-                        VIEW_BOTTOM += 1
+                        VIEW_B += 1
                         check = False
-                    if bottom_tree_idx >= len(row_data) - 1:
+                    if b_tree_idx >= len(row_data) - 1:
                         check = False
-                    bottom_tree_idx += 1
-            print(f'VIEW_BOTTOM: {VIEW_BOTTOM}')
+                    b_tree_idx += 1
 
             # ## Tree to Top
             if r_idx == 0:
                 pass
             else:
-                top_tree_idx = r_idx - 1
+                t_tree_idx = r_idx - 1
                 check = True
                 while check:
-                    print(f'[{r_idx}, {tree_idx}], [{curr_tree}], {top_tree_idx}, {grid[r_idx][top_tree_idx]}')
-                    tree_to_the_top = grid[top_tree_idx][tree_idx]
-                    if tree_to_the_top < curr_tree:
-                        VIEW_TOP += 1
+                    t_tree_val = grid[t_tree_idx][cur_tree_idx]
+                    if t_tree_val < cur_tree_val:
+                        VIEW_T += 1
                     else:
-                        VIEW_TOP += 1
+                        VIEW_T += 1
                         check = False
                     
-                    if top_tree_idx <= 0:
+                    if t_tree_idx <= 0:
                         check = False
-                    top_tree_idx -= 1
-            print(f'VIEW_TOP: {VIEW_TOP}')
+                    t_tree_idx -= 1
 
-            curr_scenic_score = VIEW_LEFT * VIEW_RIGHT * VIEW_BOTTOM * VIEW_TOP
-            
-            print(f'({r_idx}, {tree_idx}, [{curr_tree}]) - curr_scenic_score: {curr_scenic_score}')
-            # print('max_scenic_score: ', max_scenic_score)
-            
-            if curr_scenic_score > max_scenic_score:
-                max_scenic_score = curr_scenic_score
+            cur_scenic_score = VIEW_L * VIEW_R * VIEW_B * VIEW_T
+            if cur_scenic_score > max_scenic_score:
+                max_scenic_score = cur_scenic_score
         
     return max_scenic_score        
 
 
 def main():
-    # filename = '08_example.txt'  # p1: 21 || p2: ?
+    # filename = '08_example.txt'  # p1: 21 || p2: 8
     filename = '08_input.txt'
 
     puzzle_input = get_puzzle_input(filename)
     grid         = create_grid(puzzle_input)
-    visibility   = check_visibility(grid)
-    print(visibility)
+    scenic_score = get_scenic_score(grid)
+    print(scenic_score)
 
 
 if __name__ == '__main__':
