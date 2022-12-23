1ff6a4f0d75acf9fce151391c30a2fe4d0cf165a
Aaron M <thisaaronm@outlook.com>
2022-12-22 18:22:19 -0800

    major refactor and after reviewing solution from hyper-neutrino

--- 09/09_p2.py
+++ 09/09_p2.py
@@ -1,3 +1,26 @@
+'''
+This answer was HEAVILY inspired by:
+https://github.com/hyper-neutrino/advent-of-code/blob/main/2022/day09p2.py
+
+I had mixed up the tail movement from P1. In P1, for the diagonal tail motion,
+I could just move the tail into the row or column of the head.
+
+However, for some reason, it didn't click that the "head" in P2 could also move
+in a diagonal motion away from the "tail". I kept getting the wrong answer
+because I was still trying to move the tail into the same row/col as the head.
+
+        My wrong movement in P2 based off of my P1 logic
+        * * *      * * H      * T H
+        * H *  =>  * * *  =>  * * *
+        T * *      T * *      * * *
+
+        The correct movement:
+        * * *      * * H      * * H
+        * H *  =>  * * *  =>  * T *
+        T * *      T * *      * * *
+'''
+
+
 def get_puzzle_input(filename: str) -> list:
     """
     - returns puzzle input as list
@@ -8,132 +31,95 @@ def get_puzzle_input(filename: str) -> list:
     return values
 
 
-def chk_pos(idx, hdx, rope, cr):
-    if   cr == 'c': j = 0
-    elif cr == 'r': j = 1
+def get_positions(moves: list, knots: int):
+    POS   = [[0, 0]]
+    ROPE  = [[0, 0] for _ in range(knots)]
 
-    if rope[idx][j] != rope[hdx][j]:
-        rope[hdx][j] = rope[idx][j]
-
-
-def add_pos(t_pos, pos):
-    if t_pos not in pos:
-        pos.append([t_pos[0], t_pos[1]])
-
-
-def get_positions(moves: list):
-    ROPE = [[0, 0] for _ in range(10)]
-    POS  = [[0, 0]]
-    print(ROPE[9])
-
+    ## For each move (direction and number of moves in that direction)...
     for move in moves:
-        DIR = move.split()[0]
-        NUM = int(move.split()[1])
+        DIR, NUM = move.split(' ')
+        NUM = int(NUM)
 
-        while NUM > 0:
-            if DIR == 'R':
-                ROPE[0][0] += 1
-                for i in range(len(ROPE[:-1])):
-                    h = i + 1
+        ## Move the head of the rope the number of moves specified
+        for num in range(NUM):
+            if DIR == 'R': ROPE[0][0] += 1
+            if DIR == 'L': ROPE[0][0] -= 1
+            if DIR == 'D': ROPE[0][1] += 1
+            if DIR == 'U': ROPE[0][1] -= 1
 
-                    if ROPE[i][0] > ROPE[h][0] + 1:
-                        ROPE[h][0] += 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='r')
+            ## Move each knot based on the rules in the problem statement
+            ## and set H(ead) and T(ail) as their respective indices in ROPE.
+            for KNOT in range(knots - 1):
+                H = KNOT
+                T = KNOT + 1
 
-                    if ROPE[i][1] > ROPE[h][1] + 1:
-                        ROPE[h][1] += 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='c')
-                    if i == 8:
-                        add_pos(t_pos=ROPE[h], pos=POS)
+                ## CHECK COLUMNS
+                ## If the "head" (front) knot more than 2 spaces away from
+                ## the "tail" (back) knot, move the tail towards the head.
+                if abs(ROPE[H][0] - ROPE[T][0]) > 1:
 
-            if DIR == 'L':
-                ROPE[0][0] -= 1
-                for i in range(len(ROPE[:-1])):
-                    h = i + 1
+                    ## If the column positions are positive, 
+                    ## move the tail in a positive direction.
+                    if ROPE[H][0] - ROPE[T][0] > 0:
+                        ROPE[T][0] += 1
 
-                    if ROPE[i][0] < ROPE[h][0] - 1:
-                        ROPE[h][0] -= 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='r')
+                    ## If the column positions are negative, 
+                    ## move the tail in a negative direction.
+                    else:
+                        ROPE[T][0] -= 1
 
-                    if ROPE[i][1] < ROPE[h][1] - 1:
-                        ROPE[h][1] -= 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='c')
-                    if i == 8:
-                        add_pos(t_pos=ROPE[h], pos=POS)
+                    ## If the knots are in different rows, move the tail
+                    ## knot TOWARDS (***NOT INTO***) the head knot row
+                    if ROPE[H][1] != ROPE[T][1]:
+                        if ROPE[H][1] - ROPE[T][1] > 0:
+                            ROPE[T][1] += 1
+                        else:
+                            ROPE[T][1] -= 1
 
-            if DIR == 'D':
-                ROPE[0][1] += 1
-                for i in range(len(ROPE[:-1])):
-                    h = i + 1
+                ## Check rows
+                ## If the "head" (front) knot is 2 spaces away from the "tail"
+                ## (back) knot, move the tail towards the head.
+                if abs(ROPE[H][1] - ROPE[T][1]) > 1:
 
-                    if ROPE[i][1] > ROPE[h][1] + 1:
-                        ROPE[h][1] += 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='c')
+                    ## If the row positions are positive, 
+                    ## move the tail in a positive direction.
+                    if ROPE[H][1] - ROPE[T][1] > 0:
+                        ROPE[T][1] += 1
+                    ## If the row positions are negative, 
+                    ## move the tail in a negative direction.
+                    else:
+                        ROPE[T][1] -= 1
 
-                    if ROPE[i][0] > ROPE[h][0] + 1:
-                        ROPE[h][0] += 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='r')
-                    if i == 8:
-                        add_pos(t_pos=ROPE[h], pos=POS)
+                    ## If the knots are in different columns, move the tail
+                    ## knot TOWARDS (***NOT INTO***) the head knot column
+                    if ROPE[H][0] != ROPE[T][0]:
+                        if ROPE[H][0] - ROPE[T][0] > 0:
+                            ROPE[T][0] += 1
+                        else:
+                            ROPE[T][0] -= 1
 
-            if DIR == 'U':
-                ROPE[0][1] -= 1
-                for i in range(len(ROPE[:-1])):
-                    h = i + 1
-
-                    if ROPE[i][1] < ROPE[h][1] - 1:
-                        ROPE[h][1] -= 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='c')
-                    
-                    if ROPE[i][0] < ROPE[h][0] - 1:
-                        ROPE[h][0] -= 1
-                        if h == 9:
-                            print(ROPE)
-                            # print(ROPE[h])
-                        chk_pos(idx=i, hdx=h, rope=ROPE, cr='r')
-                    if i == 8:
-                        add_pos(t_pos=ROPE[h], pos=POS)
-            NUM -= 1
+                ## Check last knot (tail) position
+                ## If its position is not in POS,
+                ## append it to the list.
+                if T == knots - 1:
+                    last_knot = [ROPE[T][0], ROPE[T][1]]
+                    if last_knot not in POS:
+                        POS.append(last_knot)
     return POS
 
 
 def main():
-    # filename = '09_example1.txt' ## 1
-    filename = '09_example2.txt' ## 36
-    # filename  = '09_input.txt'
-    values    = get_puzzle_input(filename)
-    positions = get_positions(values)
-    # print(len(positions))
+    ## Test Cases
+    # filename, knots = ['09_example1.txt', 2]  ## P1
+    # filename, knots = ['09_example2.txt', 10] ## P2
 
-    too_high = [2710]
-    too_low  = []
-    if len(positions) in too_high:
-        print(f'\n{len(positions)} is too high')
-    elif len(positions) in too_low:
-        print(f'\n{len(positions)} is too low')
-    else:
-        print(f'\n{len(positions)}: Try it out')
+    ## Puzzle Input
+    # filename, knots = ['09_input.txt', 2]     ## P1
+    filename, knots = ['09_input.txt', 10]    ## P2
+
+    puzzle_input = get_puzzle_input(filename)
+    positions = get_positions(puzzle_input, knots)
+    print(len(positions))
 
 
 if __name__ == '__main__':
